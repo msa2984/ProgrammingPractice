2f68d6b0f827c264aa2352be2c92a7cd5a83ef1e
msa2984 <msa2984@rit.edu>
2018-04-21 18:26:06 -0400

    Add Recurring Character Detection
    Add a page for detecting the first recurring character in a string.

---
+++ ProgrammingPractice/MethodSpecificPages/FindFirstDuplicatePage.xaml.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Diagnostics;
+using System.Windows;
+using System.Windows.Controls;
+
+namespace ProgrammingPractice.MethodSpecificPages
+{
+    /// <summary>
+    /// Interaction logic for FindFirstDuplicatePage.xaml
+    /// </summary>
+    public partial class FindFirstDuplicatePage : Page
+    {
+        private const string MethodDescription = @"Given a user-provided string / array of characters, return the first character that occurs more than once.";
+
+        public FindFirstDuplicatePage()
+        {
+            InitializeComponent();
+            MethodSummaryTextBlock.Text = MethodDescription;
+        }
+
+        /// <summary>
+        /// Event Handler for when any of the method buttons are pressed.
+        /// </summary>
+        /// <param name="sender">Reference to the control (Button) that raised this event./param>
+        /// <param name="e">The data for the event raised.</param>
+        private void RunRecurringDetection_Click(object sender, RoutedEventArgs e)
+        {
+            switch ((sender as Button).Name)
+            {
+                case "BruteForceButton":
+                    ResultTextBox.Text = StringManipulation.BruteForceRecurringDetection(UserInputTextBox.Text);
+                    break;
+                case "DictionaryLookupButton":
+                    ResultTextBox.Text = StringManipulation.DictionaryLookupRecurringDetection(UserInputTextBox.Text);
+                    break;
+                default:
+                    throw new System.Exception("The RunStringReversal_Click event handler was called from an unexpected button!");
+            }
+            if (ResultTextBox.Text == " ")
+            {
+                ResultTextBox.Text = "First recurring character is a space.";
+            }
+            else if(String.IsNullOrEmpty(ResultTextBox.Text))
+            {
+                ResultTextBox.Text = "There are no recurring characters in the provided string.";
+            }                    
+
+            ResultsGrid.Visibility = Visibility.Visible;
+
+        }
+    }
+}
--- ProgrammingPractice/StringManipulation.cs
+++ ProgrammingPractice/StringManipulation.cs
@@ -20,7 +20,7 @@ namespace ProgrammingPractice
         {
             string output = String.Empty;
 
-            for(int i = (input.ToCharArray().Count() - 1); i >= 0; i--)
+            for (int i = (input.ToCharArray().Count() - 1); i >= 0; i--)
             {
                 output += input.ToCharArray()[i];
             }
@@ -59,11 +59,76 @@ namespace ProgrammingPractice
             for (int i = 0; i < output.Count() / 2; i++)
             {
                 char secondChar = output[output.Count() - 1 - i];
-                output[output.Count() -1 - i] = output[i];
+                output[output.Count() - 1 - i] = output[i];
                 output[i] = secondChar;
             }
 
             return new string(output);
         }
+
+        /// <summary>
+        /// Takes in a string, and returns the first repeated character. 
+        /// Because of the nested for loops, this take O(n^2) to complete,
+        /// so it is slow.
+        /// </summary>
+        /// <param name="input">The string to search for the recurring character.</param>
+        /// <returns>the first recurring character, or null if no character is repeated.</returns>
+        public static string BruteForceRecurringDetection(string input)
+        {
+            bool matchFound = false;
+            char repeatedChar = ' ';
+            for (int i = 0; i <= input.Count() - 1; i++)
+            {
+                for (int j = i + 1; j <= input.Count() - 1; j++)
+                {
+                    if (input[i] == input[j])
+                    {
+                        repeatedChar = input[i];
+                        matchFound = true;
+                        break;
+                    }
+                }
+
+                if (matchFound)
+                {
+                    repeatedChar = input[i];
+                    break;
+                }
+            }
+
+            return matchFound ? repeatedChar.ToString() : null;
+        }
+
+        /// <summary>
+        /// Takes in a string, and returns the first repeated character. 
+        /// While storing the characters in a separate List takes up
+        /// more space in memory, this function is faster because it 
+        /// doesn't require nested for loops. {O(n)}
+        /// </summary>
+        /// <param name="input">The string to search for the recurring character.</param>
+        /// <returns>the first recurring character, or null if no character is repeated.</returns>
+        public static string DictionaryLookupRecurringDetection(string input)
+        {
+            bool matchFound = false;
+            //A normal Dictionary generally does not guarantee item order 
+            //But since items aren't being removed in this function, 
+            //that isn't a concerned - otherwise SortedDictionary could be used.
+            Dictionary<char,int> occuringCharacters = new Dictionary<char, int>();
+            for (int i = 0; i <= input.Count() - 1; i++)
+            {
+                if (!occuringCharacters.ContainsKey(input[i]))
+                {
+                    occuringCharacters.Add(input[i], 1);
+                }
+                else
+                {
+                    occuringCharacters[input[i]]++;
+                    matchFound = true;
+                    break;
+                }
+            }
+            return matchFound ? occuringCharacters.First(c => c.Value > 1).Key.ToString() : null;
+        }
     }
 }
+
