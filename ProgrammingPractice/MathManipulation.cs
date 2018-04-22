a792cee6e24e38626aafc1819652a39237719548
msa2984 <msa2984@rit.edu>
2018-04-21 21:32:25 -0400

    Adding section to calculate the angles on a clock.

---
+++ ProgrammingPractice/MathManipulation.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ProgrammingPractice
+{
+    public class MathManipulation
+    {
+        /// <summary>
+        /// Number of degrees in a circle.
+        /// </summary>
+        private const int DegreesInCircle = 360;
+        /// <summary>
+        /// Number of hours on a typical clock, which 
+        /// follows AM / PM designation, not military time.
+        /// </summary>
+        private const int HoursOnClock = 12; 
+        /// <summary>
+        /// Number of minutes in an hour.
+        /// </summary>
+        private const int MinutesInHour = 60;
+        /// <summary>
+        /// Conversion factor from degrees to radians.
+        /// </summary>
+        private const double RadianPerDecimal = Math.PI / 180;
+
+        /// <summary>
+        /// Method that calculates the angle between the hour and minute 
+        /// hands on a standard 12-hour clock.
+        /// </summary>
+        /// <param name="hourInt">The hour in the day.</param>
+        /// <param name="minuteInt">The minute in the day.</param>
+        /// <returns>A Tuple with the angle in degrees, and in radians.</returns>
+        public static Tuple<decimal, decimal> CalculateClockAngle(int hourInt, int minuteInt)
+        {
+            decimal hourAngle = DegreesInCircle / HoursOnClock * (hourInt + Decimal.Divide(minuteInt, MinutesInHour));
+            decimal minuteAngle = DegreesInCircle / MinutesInHour * minuteInt;
+
+            decimal degreeDifference = Math.Abs(hourAngle - minuteAngle);
+            decimal radianDifference = degreeDifference * (decimal)RadianPerDecimal;
+
+            return new Tuple<decimal, decimal>(degreeDifference, radianDifference);
+        }
+    }
+}
---
+++ ProgrammingPractice/MethodSpecificPages/CalculateClockAngle.xaml.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace ProgrammingPractice.MethodSpecificPages
+{
+    /// <summary>
+    /// Interaction logic for CalculateClockAngle.xaml
+    /// </summary>
+    public partial class CalculateClockAngle : Page
+    {
+        private const int MaximumHour = 12;
+        private const int MaximumMinute = 59;
+        private const string MethodDescription = @"Given a time of day, return the angle between the hour and minute hand on a standard clock.";
+
+        public CalculateClockAngle()
+        {
+            InitializeComponent();
+            MethodSummaryTextBlock.Text = MethodDescription;
+            
+            for(int i = 1; i <= MaximumHour; i++)
+            {
+                HourComboBox.Items.Add(i);
+            }
+
+            for (int i = 0; i <= MaximumMinute; i++)
+            {
+                MinuteComboBox.Items.Add(i);
+            }
+        }
+
+        /// <summary>
+        /// Event Handler for when any of the method buttons are pressed.
+        /// </summary>
+        /// <param name="sender">Reference to the control (Button) that raised this event./param>
+        /// <param name="e">The data for the event raised.</param>
+        private void CalculateHandAngle_Click(object sender, RoutedEventArgs e)
+        {
+            bool wasTimeParsed = true; 
+            int hour = 0;
+            int minute = 0;
+            wasTimeParsed &= Int32.TryParse(HourComboBox.SelectedItem.ToString(), out hour);
+            wasTimeParsed &= Int32.TryParse(MinuteComboBox.SelectedItem.ToString(), out minute);
+
+            if (wasTimeParsed)
+            {
+                Tuple<decimal, decimal> calculationResults = MathManipulation.CalculateClockAngle(hour, minute);
+                DegreeDifferenceTextBox.Text = calculationResults.Item1.ToString("###.###");
+                RadianDifferenceTextBox.Text = calculationResults.Item2.ToString("###.###");
+                ResultsGrid.Visibility = Visibility.Visible;
+            }
+            else
+            {
+                MessageBox.Show("A time of day was not selected! The method cannot continue.", "Calculate Clock Angle");
+            }
+        }
+    }
+}
